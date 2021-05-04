8eca7d37342183716ebe67073a480488d2a3f2fb
offetuoso <offetuoso@gmail.com>
2021-05-04 10:27:59 +0900

    2021-05-04

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,25 @@ git push origin main
 # Come Back up to the Project Root
 cd ..
 
+# Go To Public folder
+cd themes/hugo-theme-codex
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin theme
+
+# Come Back up to the Project Root
+cd ..
+cd ..
+
 
 # blog 저장소 Commit & Push
 git add .
