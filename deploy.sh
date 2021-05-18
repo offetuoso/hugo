94883e75fbb9834ef6b59bb4b6ea1b88675581ad
offetuoso <offetuoso@gmail.com>
2021-05-18 11:20:36 +0900

    테스트 커밋

--- deploy.sh
+++ deploy.sh
@@ -26,26 +26,6 @@ git push origin main
 # Come Back up to the Project Root
 cd ..
 
-# Go To Public folder
-cd themes/hugo-theme-codex
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
-cd ..
-
-
 # blog 저장소 Commit & Push
 git add .
 
